ed712f39cf09cfe7d81aa7d73c182a7286da1616
JessicaLPontes <jessicalizandrapontes@gmail.com>
2024-12-17 11:15:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // URL do cardápio (hospedado no GitHub Pages)
-const cardapioURL = "https://seu-usuario.github.io/cardapio-digital/";
+const cardapioURL = "https://github.com/JessicaLPontes/cardapiodigital/blob/main/Card%C3%A1pio%20teste.pdf";
 
 const qrcodeContainer = document.getElementById("qrcode");
 
